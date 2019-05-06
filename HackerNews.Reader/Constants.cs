a73a53dfd72f8390e22795327713e47bda94a4f1
Nick <nicholas.patsaris@gmail.com>
2019-05-06 23:20:38 +0100

    added constants

---
+++ HackerNews.Reader/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerNews.Reader
+{
+	/// <summary>
+	/// Specifies type of posts to retrieve
+	/// </summary>
+	public enum PostType
+	{
+		Stories,
+		Jobs,
+		Ask,
+		Show
+	}
+
+	/// <summary>
+	/// Specifies how deep to go when retrieving comments for a story
+	/// </summary>
+	public enum CommentLevel
+	{
+		None, // do not retrieve any comments
+		FirstLevel, // only comments directly to the story, no replies
+		Full // all comments
+	}
+
+	public class Constants
+	{
+		public const string HackerNewsTopStoriesUri = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+		public const string HackerNewsJobsUri = "https://hacker-news.firebaseio.com/v0/jobstories.json?print=pretty";
+		public const string HackernewsAskUri = "https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty";
+		public const string HackerNewsShowUri = "https://hacker-news.firebaseio.com/v0/showstories.json?print=pretty";
+
+	}
+}
